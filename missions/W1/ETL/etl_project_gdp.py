13c5fffbd29a6c388513231f57ebe96d8da6d3f5
nothingmin <wnl1012@naver.com>
2024-07-05 15:21:39 +0900

    refactor(missions/W1/ETL): separte code and data

--- missions/W1/ETL/etl_project_gdp.py
+++ missions/W1/ETL/etl_project_gdp.py
@@ -1,4 +1,5 @@
 import collections
+import json
 import logging
 
 import pandas as pd
@@ -10,63 +11,6 @@ collections.Callable = collections.abc.Callable
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename='etl_project_log.txt', encoding='utf-8', level=logging.DEBUG,
                     format='%(asctime)s,%(message)s', datefmt='%Y-%B-%d-%H-%M-%S')
-country_region = {'Afghanistan': 'Asia', 'Armenia': 'Europe', 'Azerbaijan': 'Europe', 'Bahrain': 'Asia',
-                  'Bangladesh': 'Asia', 'Bhutan': 'Asia', 'Brunei': 'Asia', 'Cambodia': 'Asia', 'China': 'Asia',
-                  'Cyprus': 'Europe', 'Georgia': 'Europe', 'India': 'Asia', 'Indonesia': 'Asia', 'Iran': 'Asia',
-                  'Iraq': 'Asia', 'Israel': 'Asia', 'Japan': 'Asia', 'Jordan': 'Asia', 'Kazakhstan': 'Europe',
-                  'Kuwait': 'Asia', 'Kyrgyzstan': 'Asia', 'Laos': 'Asia', 'Lebanon': 'Asia', 'Malaysia': 'Asia',
-                  'Maldives': 'Asia', 'Mongolia': 'Asia', 'Myanmar': 'Asia', 'Nepal': 'Asia',
-                  'North Korea': 'Asia', 'Oman': 'Asia', 'Pakistan': 'Asia', 'Palestine': 'Asia',
-                  'Philippines': 'Asia', 'Qatar': 'Asia', 'Saudi Arabia': 'Asia', 'Singapore': 'Asia',
-                  'South Korea': 'Asia', 'Sri Lanka': 'Asia', 'Syria': 'Asia', 'Taiwan': 'Asia',
-                  'Tajikistan': 'Asia', 'Thailand': 'Asia', 'Timor-Leste': 'Asia', 'Turkey': 'Europe',
-                  'Turkmenistan': 'Asia', 'United Arab Emirates': 'Asia', 'Uzbekistan': 'Asia', 'Vietnam': 'Asia',
-                  'Hong Kong': 'Asia', 'Macau': 'Asia', 'East Timor': 'Asia',
-                  'Yemen': 'Asia', 'Antigua and Barbuda': 'North America', 'Bahamas': 'North America',
-                  'Barbados': 'North America', 'Belize': 'North America', 'Canada': 'North America',
-                  'Costa Rica': 'North America', 'Cuba': 'North America', 'Dominica': 'North America',
-                  'Dominican Republic': 'North America', 'El Salvador': 'North America',
-                  'Grenada': 'North America', 'Guatemala': 'North America', 'Haiti': 'North America',
-                  'Honduras': 'North America', 'Jamaica': 'North America', 'Mexico': 'North America',
-                  'Nicaragua': 'North America', 'Panama': 'North America',
-                  'Saint Kitts and Nevis': 'North America', 'Saint Lucia': 'North America',
-                  'Saint Vincent and the Grenadines': 'North America', 'Trinidad and Tobago': 'North America',
-                  'Puerto Rico': 'North America',
-                  'United States': 'North America', 'Albania': 'Europe', 'Andorra': 'Europe', 'Austria': 'Europe',
-                  'Belarus': 'Europe', 'Belgium': 'Europe', 'Bosnia and Herzegovina': 'Europe',
-                  'Bulgaria': 'Europe', 'Croatia': 'Europe', 'Czech Republic': 'Europe', 'Denmark': 'Europe',
-                  'Estonia': 'Europe', 'Finland': 'Europe', 'France': 'Europe', 'Germany': 'Europe',
-                  'Greece': 'Europe', 'Hungary': 'Europe', 'Iceland': 'Europe', 'Ireland': 'Europe',
-                  'Italy': 'Europe', 'Kosovo': 'Europe', 'Latvia': 'Europe', 'Liechtenstein': 'Europe',
-                  'Lithuania': 'Europe', 'Luxembourg': 'Europe', 'Malta': 'Europe', 'Moldova': 'Europe',
-                  'Monaco': 'Europe', 'Montenegro': 'Europe', 'Netherlands': 'Europe', 'North Macedonia': 'Europe',
-                  'Norway': 'Europe', 'Poland': 'Europe', 'Portugal': 'Europe', 'Romania': 'Europe',
-                  'Russia': 'Europe', 'San Marino': 'Europe', 'Serbia': 'Europe', 'Slovakia': 'Europe',
-                  'Slovenia': 'Europe', 'Spain': 'Europe', 'Sweden': 'Europe', 'Switzerland': 'Europe',
-                  'Ukraine': 'Europe', 'United Kingdom': 'Europe', 'Vatican City': 'Europe',
-                  'Argentina': 'South America', 'Aruba': 'South America', 'Bolivia': 'South America',
-                  'Brazil': 'South America',
-                  'Chile': 'South America', 'Colombia': 'South America', 'Ecuador': 'South America',
-                  'Guyana': 'South America', 'Paraguay': 'South America', 'Peru': 'South America',
-                  'Suriname': 'South America', 'Uruguay': 'South America', 'Venezuela': 'South America',
-                  'Algeria': 'Africa', 'Angola': 'Africa', 'Benin': 'Africa', 'Botswana': 'Africa',
-                  'Burkina Faso': 'Africa', 'Burundi': 'Africa', 'Cabo Verde': 'Africa', 'Cameroon': 'Africa',
-                  'Central African Republic': 'Africa', 'Chad': 'Africa', 'Comoros': 'Africa', 'Congo': 'Africa',
-                  'DR Congo': 'Africa', 'Cape Verde': 'Africa', 'São Tomé and Príncipe': 'Africa',
-                  'Djibouti': 'Africa', 'Egypt': 'Africa', 'Equatorial Guinea': 'Africa', 'Eritrea': 'Africa',
-                  'Eswatini': 'Africa', 'Ethiopia': 'Africa', 'Gabon': 'Africa', 'Gambia': 'Africa',
-                  'Ghana': 'Africa', 'Guinea': 'Africa', 'Guinea-Bissau': 'Africa', 'Ivory Coast': 'Africa',
-                  'Kenya': 'Africa', 'Lesotho': 'Africa', 'Liberia': 'Africa', 'Libya': 'Africa',
-                  'Madagascar': 'Africa', 'Malawi': 'Africa', 'Mali': 'Africa', 'Mauritania': 'Africa',
-                  'Mauritius': 'Africa', 'Morocco': 'Africa', 'Mozambique': 'Africa', 'Namibia': 'Africa',
-                  'Niger': 'Africa', 'Nigeria': 'Africa', 'Rwanda': 'Africa', 'Sao Tome and Principe': 'Africa',
-                  'Senegal': 'Africa', 'Seychelles': 'Africa', 'Sierra Leone': 'Africa', 'Somalia': 'Africa',
-                  'South Africa': 'Africa', 'South Sudan': 'Africa', 'Sudan': 'Africa', 'Tanzania': 'Africa',
-                  'Togo': 'Africa', 'Tunisia': 'Africa', 'Uganda': 'Africa', 'Zambia': 'Africa',
-                  'Zimbabwe': 'Africa', 'Australia': 'Oceania', 'Fiji': 'Oceania', 'Kiribati': 'Oceania',
-                  'Marshall Islands': 'Oceania', 'Micronesia': 'Oceania', 'Nauru': 'Oceania',
-                  'New Zealand': 'Oceania', 'Palau': 'Oceania', 'Papua New Guinea': 'Oceania', 'Samoa': 'Oceania',
-                  'Solomon Islands': 'Oceania', 'Tonga': 'Oceania', 'Tuvalu': 'Oceania', 'Vanuatu': 'Oceania'}
 
 
 def extract():
@@ -133,13 +77,15 @@ def transform(imf_gdp):
 
 def get_region_column(imf_gdp):
     region = []
-    for row in imf_gdp.iterrows():
-        country = row[1]['Country']
-        if country in country_region:
-            region.append(country_region[country])
-        else:
-            region.append(None)
-            logging.debug("region for {} not found".format(country))
+    with open('country_region.json') as json_file:
+        country_region = json.load(json_file)
+        for row in imf_gdp.iterrows():
+            country = row[1]['Country']
+            if country in country_region:
+                region.append(country_region[country])
+            else:
+                region.append(None)
+                logging.debug("region for {} not found".format(country))
     return pd.DataFrame(region, columns=['Region'])
 
 
